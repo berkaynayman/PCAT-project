b7351ad58188882ed1b47a554faef2fb4b14a6f5
berkaynayman <berkaynayman4@gmail.com>
2022-07-02 23:54:12 +0300

    express, nodemon added to project.

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-  console.log('function working');
-};
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo name',
+    desc: 'Photo desc'
+  }
+  res.send(photo)
+  //res.send('HELLO')
+})
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`)
+})
