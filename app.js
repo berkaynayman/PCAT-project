21cda2d31d4446c394f036c540411e69e47aef06
berkaynayman <berkaynayman4@gmail.com>
2022-07-10 20:46:33 +0300

    Mongoose. MongoDB connect. Body request. Model and dynamic content.

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const ejs = require('ejs')
 const path = require('path')
+const Photo = require('./models/Photo')
 
 const app = express()
+
+mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs')
 
 // MIDDLEWARES
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 //ROUTES
-app.get('/', (req, res) => {
-  //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-  res.render('index') // views/index.ejs
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  console.log(photos)
+  res.render('index', {
+    photos
+  }) // views/index.ejs
 })
 app.get('/about', (req, res) => {
   res.render('about')
 })
 app.get('/add', (req, res) => {
   res.render('add')
+})
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
+  res.redirect('/')
 })
 
 const port = 3000
